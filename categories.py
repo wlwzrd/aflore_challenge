d604b274496f864065cf93e6e09ad1aa96ae2856
Mario Gutierrez <mfdogutierrez@gmail.com>
2017-03-06 12:15:30 +0000

    created get node and childNodes

--- categories.py
+++ categories.py
@@ -4,51 +4,82 @@ import getopt
  
 from xml.dom.minidom import parse, parseString
 from ebayHelper import getEbayXml
-from sqlHelper import createConnection, runSQL, DB_name, SQL_createTable, insertSQL
+from sqlHelper import createConnection, runSQL, DB_name, SQL_createNestedTable, SQL_falseParent, insertNode, countSQL,getCategories
 
 __author__ = "Mario Gutierrez/wlwzrd"
 __email__ = "mfdogutierrez@gmail.com"
+
+def sortCategoriesByLevel(categoriesXml):
+    categoriesList =[]
+    for item in categoriesXml:
+        try:
+            f_bestOfferEnabled = (item.getElementsByTagName('BestOfferEnabled')[0].childNodes[0].data).encode('utf-8')
+            f_categoryID = (item.getElementsByTagName('CategoryID')[0].childNodes[0].data).encode('utf-8')
+            f_categoryLevel = (item.getElementsByTagName('CategoryLevel')[0].childNodes[0].data).encode('utf-8')
+            f_categoryName = (item.getElementsByTagName('CategoryName')[0].childNodes[0].data).encode('utf-8')
+            f_categoryParentID = (item.getElementsByTagName('CategoryParentID')[0].childNodes[0].data).encode('utf-8')
+            categoriesList.append({"bOE":f_bestOfferEnabled,"cID":f_categoryID,"cLevel":f_categoryLevel,"cName":f_categoryName,"cParentID":f_categoryParentID})
+        except Exception,err:
+            print "CATEGORIES.PY:", err,  item.getElementsByTagName('CategoryID')[0].childNodes[0].data
+    categoriesOrdered = sorted(categoriesList, key = lambda cat: (cat['cLevel']))
+    return categoriesOrdered
+
+def genJson(rows):
+    data = {"categoryID":"100548", "categoryName":"Toys", "tree": {"name":"Hello","children":[{"name":"Child 1"},{"name":"Child 2"}]}}
+    categoriesList = [] 
+    for row in rows:
+        j_cID = row[0]
+        j_cName = row[1]
+        j_cLevel = row[2]
+        j_cParentID = row[3]
+        categoriesList.append({"cID":j_cID,"cName":j_cName,"cLevel":j_cLevel,"cParentID":j_cParentID})
+    categoriesOrdered = sorted(categoriesList, key = lambda cat: (cat['cLevel']))
+    
+    
 
 def build():
     xmlOut = getEbayXml()
     response = parseString(xmlOut)
     categories = response.getElementsByTagName('Category')
-    # categories[numero de categorias].childNodes[0..5].childNodes[0].data
+    categoriesOrdered = sortCategoriesByLevel(categories)
     if os.path.isfile(DB_name):
         os.remove(DB_name)
-    conn = createConnection(DB_name)
+    conn = createConnection(DB_name)    
     if conn is not None:
-        status = runSQL(conn, SQL_createTable)
-        if status:
-            incomplete = []
-            success = 0
-            fails = 0
-            for item in categories:
-                try:
-                    f_bestOfferEnabled = (item.getElementsByTagName('BestOfferEnabled')[0].childNodes[0].data).encode('utf-8')
-                    f_categoryID = (item.getElementsByTagName('CategoryID')[0].childNodes[0].data).encode('utf-8')
-                    f_categoryLevel = (item.getElementsByTagName('CategoryLevel')[0].childNodes[0].data).encode('utf-8')
-                    f_categoryName = (item.getElementsByTagName('CategoryName')[0].childNodes[0].data).encode('utf-8')
-                    f_categoryParentID = (item.getElementsByTagName('CategoryParentID')[0].childNodes[0].data).encode('utf-8')
-                    insert = runSQL(conn, insertSQL(f_categoryID,f_categoryName,f_categoryLevel,f_bestOfferEnabled,f_categoryParentID))
-                    if insert:
-                        success += 1
-                    else:
-                        fails += 1
-                except:
-                    incomplete.append({"Category ID":f_categoryID})
-            print "DATABASE SUCCESSFULLY CREATED!"
-            print "---------------------------------"
-            print "Success Insertions: ", success
-            print "Fail Insertions: ", fails
-            print "Incomplete Categories: ", len(incomplete) 
-            print "---------------------------------"
-            return True
-        else: 
-            print "Can't create table..."
+        runSQL(conn, SQL_createNestedTable)
+        runSQL(conn, SQL_falseParent)
+        for itemO in categoriesOrdered:
+            try:
+                insertNode(conn, itemO["cID"],itemO["cName"],itemO["cLevel"],itemO["bOE"],itemO["cParentID"])
+            except Exception,err:
+                print "INSERT:", err, itemO["cID"]
+        c = countSQL(conn)
+        print "---------------------------------"
+        print "Database Successfully Created!"
+        print "Created: ", c
+        print "---------------------------------"
     else:
         print("Error while creating a connection to the database.")
 
+def createData(categoryID):
+    if os.path.isfile(DB_name):
+        conn = createConnection(DB_name)
+        if conn is not None:
+            algo = getCategories(conn,categoryID)
+            for item in algo:
+                print item[0],item[1],item[2]
+            print "GENERATED"
+            #algo2 = genJson(algo)
+            #createHtml()
+            #createJs(algo2)
+        else:
+            #No se puedo establecer conexion
+            print("Error while creating a connection to the database.")
+    else:
+        #NO hay BAse de datos
+        print("FATAL: Seens like there is not a database.")
+
+         
 def main(argv):
     category_id = ""
     try:
@@ -61,11 +92,12 @@ def main(argv):
             print './categories --rebuild / --render <category_id>'
             sys.exit()
         elif opt == '--rebuild':
-            print "BUILDIND DATABASE FROM EBAY API..."
+            print "Just wait a momment, something magical is happening in the database..."
             build()
         elif opt  == '--render':
             category_id = arg
-            print "CAtegoria buscada: ", category_id
+            print "Categoria buscada: ", category_id
+            createData(category_id)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- sqlHelper.py
+++ sqlHelper.py
@@ -5,26 +5,18 @@ __author__ = "Mario Gutierrez/wlwzrd"
 __email__ = "mfdogutierrez@gmail.com"
 
 DB_name = 'ebay_categories.db'
-SQL_createTable = """ CREATE TABLE IF NOT EXISTS category_T (
+
+SQL_createNestedTable = """ CREATE TABLE IF NOT EXISTS categoryN (
                                         category_ID INTEGER PRIMARY KEY,
                                         category_Name TEXT NOT NULL,
                                         category_Level INTEGER,
                                         best_OfferEnabled TEXT,
                                         category_ParentID INTEGER,
-                                        FOREIGN KEY(category_ParentID) REFERENCES category_T(category_ID)
-                                    ); """
+                                        lft INTEGER NOT NULL,
+                                        rgt INTEGER NOT NULL,
+                                        FOREIGN KEY(category_ParentID) REFERENCES category_T(category_ID));"""
 
-def insertSQL(cID,cName,cLevel,bOE,cParentID):
-    """ create an insert sql
-    :param cID: Category ID
-    :param cName: Category Name
-    :param cLevel: Category Level
-    :param bOE: Best Offer Enabled
-    :param cParentID: Category Parent ID
-    :return: created sql statement
-    """
-    sql = 'INSERT INTO category_T VALUES (' + cID + ',"' + cName + '",' + cLevel + ',"' + bOE + '",' + cParentID + ')'
-    return sql
+SQL_falseParent = """INSERT INTO categoryN VALUES (0,'FalseParent',0,'False',0,1,2)"""
 
 def createConnection(db):
     """ create a database connection to the SQLite3 database
@@ -35,8 +27,7 @@ def createConnection(db):
         conn = sqlite3.connect(db)
         return conn
     except Error as e:
-        print("Error while connecting to the  database")
-        print e
+        print "ERROR: Ccconnecting to the  database: ", e
     return None 
 
 def runSQL(conn, sql):
@@ -44,13 +35,60 @@ def runSQL(conn, sql):
     :param conn: Connection object
     :param sql: a SQL query
     """
-    log = []
     try:
         c = conn.cursor()
         c.execute(sql)
         conn.commit()
-        return True
     except Error as e:
-        log.append(["Error while running SQL in database",e,sql])
-        print log
-    return False
+        print "ERROR runSQL: ", e
+
+def countSQL(conn):
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT COUNT(*) FROM categoryN")
+        r = cur.fetchall()
+        count = r[0][0]
+        return count
+    except Error as e:
+        print e
+
+def insertNode(conn,cID,cName,cLevel,bOE,cParentID):
+    try:
+        print "INSERTING: ", cID,cName, cLevel, bOE, cParentID
+        cursor = conn.cursor()
+        if int(cLevel) == 1:
+            getFalseParent = ("SELECT lft, rgt FROM categoryN WHERE lft = 1")
+            cursor.execute(getFalseParent)
+            rows = cursor.fetchall()
+        elif int(cLevel) > 1:
+            getParentNode = "SELECT lft, rgt FROM categoryN WHERE category_ID = " + str(cParentID) + ";"
+            cursor.execute(getParentNode)
+            rows = cursor.fetchall()
+        lftP = rows[0][0]
+        rgtP = rows[0][1]
+        sqlUpdateRgt = "UPDATE categoryN SET rgt = rgt+2 WHERE rgt >= " + str(rgtP) +  ";"
+        sqlUpdateLft = "UPDATE categoryN SET lft = lft+2 WHERE lft >= " + str(rgtP) +  ";"
+        sqlInsertNew = 'INSERT INTO categoryN VALUES ('+ cID +',"'+ cName +'",'+ cLevel +',"'+ bOE +'",'+ cParentID +','+ str(rgtP) +','+ str(rgtP+1) +')'
+        cursor.execute(sqlUpdateRgt)
+        cursor.execute(sqlUpdateLft)
+        cursor.execute(sqlInsertNew)
+        conn.commit()
+    except Error as e:
+        print "NODE PARENT:", e,  cID
+
+def getCategories(conn, categoryID):
+    """ 
+    """
+    try:
+        cur = conn.cursor()
+        getParentNode = "SELECT lft, rgt FROM categoryN WHERE category_ID = " + str(categoryID) + ";"
+        cur.execute(getParentNode)
+        rows = cur.fetchall()
+        lftP = rows[0][0]
+        rgtP = rows[0][1]
+        sqlGetAll = "SELECT category_ID, category_Name, category_Level, category_ParentID  FROM categoryN WHERE lft BETWEEN " + str(lftP) + " AND " + str(rgtP)
+        cur.execute(sqlGetAll)
+        rows = cur.fetchall()
+        return rows
+    except Error as e:
+        print e
